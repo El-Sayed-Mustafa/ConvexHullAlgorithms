53fa3ffa9730f0c1943e6a72727792925ffdcb8d
El-Sayed-Mustafa <engelsayedmustafa123@gmail.com>
2023-11-16 21:04:15 +0200

    Update ExtremePoints

--- CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
+++ CGAlgorithms/Algorithms/ConvexHull/ExtremePoints.cs
@@ -1,5 +1,4 @@
 ﻿using CGUtilities;
-using System;
 using System.Collections.Generic;
 using System.Reflection;
 
@@ -7,37 +6,40 @@ namespace CGAlgorithms.Algorithms.ConvexHull
 {
     public class ExtremePoints : Algorithm
     {
-        public override void Run(List<Point> points, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
+        public override void Run(List<Point> inputPoints, List<Line> lines, List<Polygon> polygons, ref List<Point> outPoints, ref List<Line> outLines, ref List<Polygon> outPolygons)
         {
-
-            if (points.Count <= 3)
+            // Check if the number of input points is less than or equal to 3
+            // If true, the input points are the convex hull
+            if (inputPoints.Count <= 3)
             {
-                outPoints = points;
+                outPoints = inputPoints;
                 return;
             }
 
-            Point point1, point2, point3, points4;
-            outPoints = new List<Point> (points);
+            // Copy the input points to the output points list
+            outPoints = new List<Point>(inputPoints);
             bool flag = false;
+
             // Iterate through each point
-            for (int i = 0; i < points.Count; i++)
+            for (int i = 0; i < inputPoints.Count; i++)
             {
-              
                 // Iterate through each pair of points (j, k)
-                for (int j = 0; j < points.Count; j++)
+                for (int j = 0; j < inputPoints.Count; j++)
                 {
                     if (flag == true)
                     {
+                        // If the flag is set, exit the loop
                         flag = false;
                         break;
                     }
-                    for (int k = 0; k < points.Count; k++)
+
+                    for (int k = 0; k < inputPoints.Count; k++)
                     {
                         if (j != i && k != i && j != k)
                         {
                             // Find a point m other than j and k
                             int m;
-                            for (m = 0; m < points.Count; m++)
+                            for (m = 0; m < inputPoints.Count; m++)
                             {
                                 if (m != j && m != k && m != i)
                                 {
@@ -46,22 +48,19 @@ namespace CGAlgorithms.Algorithms.ConvexHull
                             }
 
                             // Check if the current point (i) lies inside the triangle formed by other points (j, k, m)
-                            Enums.PointInPolygon position = HelperMethods.PointInTriangle(points[i], points[j], points[k], points[m]);
-                            point1 = points[i];
-                            point2 = points[j];
-                            point3 = points[k];
-                            points4 = points[m];
+                            Enums.PointInPolygon position = HelperMethods.PointInTriangle(inputPoints[i], inputPoints[j], inputPoints[k], inputPoints[m]);
 
-                            if (outPoints.Count==3)
+                            // Check if we already have 3 points in the convex hull
+                            if (outPoints.Count == 3)
                             {
                                 flag = true;
-
                                 break;
                             }
+
+                            // If the current point lies inside or on the edge of the triangle, remove it from the convex hull
                             if (position == Enums.PointInPolygon.Inside || position == Enums.PointInPolygon.OnEdge)
                             {
-                                // If it lies inside, remove the current point from the result
-                                outPoints.Remove(points[i]);
+                                outPoints.Remove(inputPoints[i]);
                                 flag = true;
                                 break;
                             }
@@ -69,7 +68,6 @@ namespace CGAlgorithms.Algorithms.ConvexHull
                     }
                 }
             }
-
         }
 
         public override string ToString()
